6e98d2a09e58eb26a9bfb10938aac4cc89009921
Vaibhav Deokar <deokar.vaibhav@outlook.com>
2021-08-28 02:06:31 +0530

    Add Digit Model

--- src/classify_proj/settings.py
+++ src/classify_proj/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,7 +42,7 @@ INSTALLED_APPS = [
     #add
     'rest_framework', 
     'corsheaders',
-    'digit',
+    'digits',
 ]
 
 MIDDLEWARE = [
